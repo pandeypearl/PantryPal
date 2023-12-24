1575da9f4eb44ef6f8a7e69fe58d40dcc533bb46
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-24 16:55:45 +0200

    Login Page React Hook form

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,45 +1,37 @@
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
 import '../../styles/auth.scss';
 import AuthCreators from '../../components/AuthCreators';
 
 const LogIn = () => {
-    const [username, setUsername] = useState(''); 
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [error, setError]= useState(null);
-    const [success, setSuccess] = useState(false);
     const navigate = useNavigate();
+    const { handleSubmit, control, setError, formState: { errors } } = useForm();
 
-    const handleLogin = async () => {
+    const onSubmit = async (data) => {
         try{
-            setLoading(true);
             const response = await fetch('http://localhost:8000/login', {
                 method: 'POST',
                 headers: {
                     'Content Type': 'application/json',
                 },
-                body: JSON.stringify({
-                    username,
-                    password,
-                }),
+                body: JSON.stringify(data),
             });
 
-            const data = await response.json();
+            const responseData = await response.json();
 
-            if (data.success) {
-                setSuccess(true);
-                setError(null);
+            if (responseData.success) {
                 navigate.push('/')
             } else {
-                setSuccess(false);
-                setError(data || 'Login failed');
+                console.error('Error during login:', responseData.errors);
+
+                Object.keys(responseData.errors).forEach((field) => {
+                    const error = { message: responseData.errors[field] };
+                    setError(field, 'backend', error);
+                });
             }
         } catch (error) {
             console.error('Error during login:', error);
-            setError('Network error.');
-        } finally {
-            setLoading(false);
+            setError('network', 'backend', 'Network error.');
         }
     };
 
@@ -49,34 +41,33 @@ const LogIn = () => {
                 <div className='auth-container'>
                     <h2>Log In</h2>
                     <hr />
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div>
                             <label>Username</label>
-                            <input 
-                                type='text' 
-                                value={username} 
-                                onChange={(e) => setUsername(e.target.value)} 
-                                placeholder='your username here...'
+                            <Controller
+                                name='username'
+                                control={control}
+                                defaultValue=''
+                                render={({ field }) => <input {...field} placeholder='your username here...' />}
                             />
+                            {errors.username && <div style={{ color: 'red' }}>{errors.username.message}</div>}
                         </div>
 
                         <div>
                             <label>Password</label>
-                            <input 
-                                type='password' 
-                                value={password} onChange={(e) => setPassword(e.target.value)}
-                                placeholder='your password here...'
-                            />    
+                            <Controller
+                                name='password'
+                                control={control}
+                                defaultValue=''
+                                render={({ field }) => <input {...field} type='password' placeholder='your password here...' />}
+                            />
+                            {errors.password && <div style={{ color: 'red' }}>{errors.password.message}</div>}   
                         </div>
 
-                        
-                        <button type='button' onClick={handleLogin} disabled={loading}>
-                        {loading ? 'Logging in...': 'Log in'} 
-                        </button>
-                        {success && <p>Log in successful! Redirecting...</p>}
-                        {error && <p style={{ color: 'red'}}>{error}</p>}
+                        <button type='submit'>Log in</button>
+                        {errors.network && <p style={{ color: 'red' }}>{errors.network.message}</p>}
                     </form>
-                    <p><small>Forgot your password? <Link to='/login' className='auth-link'>Reset password</Link>.</small></p>
+                    <p><small>Forgot your password? <Link to='/' className='auth-link'>Reset password</Link>.</small></p>
                 </div>
             </div>
             <AuthCreators />
