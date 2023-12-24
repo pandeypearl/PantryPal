bcfdadf25ba54a55587c0985fc82680cb307f14c
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-24 18:41:07 +0200

    UseLocation hook for auth buttons in TopNav Component

--- frontend/src/components/TopNav/index.js
+++ frontend/src/components/TopNav/index.js
@@ -1,13 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import './index.scss';
 
 const TopNav = () => {
+    const location = useLocation();
+    const isAuthPage = location.pathname === '/signup' || location.pathname === '/login';
+
     return (
         <div className='top-nav'>
             <Link to='/' className='logo'>PantryPal</Link>
             <nav>
-                <NavLink to='/signup' className='auth-btn'>sign up</NavLink>
-                <NavLink to='/login' className='auth-btn'>log in</NavLink>
+                {!isAuthPage && <NavLink to='/signup' className='auth-btn'>sign up</NavLink>}
+                {!isAuthPage && <NavLink to='/login' className='auth-btn'>log in</NavLink>}
             </nav>
         </div>
     )
