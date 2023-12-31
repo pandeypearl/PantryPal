9e3e08552479973ad06882d79a5c499c93451633
Pretty Pandey <prettypandeypearl@gmail.com>
2023-12-31 18:30:03 +0200

    Backend api authentication setup

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,13 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from .serializers import UserProfileSerializer
+from users.models import UserProfile
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -28,4 +31,13 @@ def getRoutes(request):
         '/api/token',
         '/api-token/refresh',
     ]
-    return Response(routes) 
+    return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfiles(request):
+    user = request.user
+    profiles = user.user_profile_set.all()
+    serializer = UserProfileSerializer(profiles, many=True)
+    return Response(serializer.data)
